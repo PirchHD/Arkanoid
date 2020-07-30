7187cb0241775cf4e5957fb25beaa1f871f08d00
Prochal Szymon <34886043+PirchHD@users.noreply.github.com>
2020-07-30 17:22:03 +0200

    add ball moves

--- Arkanoid/Author.h
+++ Arkanoid/Author.h
@@ -34,6 +34,9 @@ namespace Arkanoid {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::Button^ button1;
+	protected:
 
 	private:
 		/// <summary>
@@ -48,12 +51,58 @@ namespace Arkanoid {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"Author";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->SuspendLayout();
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Century Gothic", 9.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label1->Location = System::Drawing::Point(40, 9);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(199, 68);
+			this->label1->TabIndex = 0;
+			this->label1->Text = L"Arkanoid \nemail: proszym2000@interia.pl\n or szympirchhd@gmail.com\n autor: Prochal"
+				L" Szymon\n my web: <one day it will be a link>";
+
+			this->label1->TextAlign = System::Drawing::ContentAlignment::MiddleCenter;
+			this->label1->Click += gcnew System::EventHandler(this, &Author::label1_Click);
+			// 
+			// button1
+			// 
+			this->button1->BackColor = System::Drawing::SystemColors::ControlDark;
+			this->button1->Location = System::Drawing::Point(80, 97);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(116, 30);
+			this->button1->TabIndex = 1;
+			this->button1->Text = L"Close";
+			this->button1->UseVisualStyleBackColor = false;
+			this->button1->Click += gcnew System::EventHandler(this, &Author::button1_Click);
+			// 
+			// Author
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(288, 139);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->label1);
+			this->Name = L"Author";
+			this->Text = L"Arkanoid";
+			this->Load += gcnew System::EventHandler(this, &Author::Author_Load);
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
 		}
 #pragma endregion
+	private: System::Void Author_Load(System::Object^ sender, System::EventArgs^ e) {
+	}
+	private: System::Void label1_Click(System::Object^ sender, System::EventArgs^ e) {
+	}
+
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+		this->Close();
+	}
 	};
 }
--- Arkanoid/Game.h
+++ Arkanoid/Game.h
@@ -34,12 +34,19 @@ namespace Arkanoid {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::PictureBox^ ball;
+	protected:
+
+	private: System::Windows::Forms::Timer^ timer;
+	protected:
+
+	private: System::ComponentModel::IContainer^ components;
 
 	private:
 		/// <summary>
 		/// Wymagana zmienna projektanta.
 		/// </summary>
-		System::ComponentModel::Container ^components;
+
 
 #pragma region Windows Form Designer generated code
 		/// <summary>
@@ -48,12 +55,68 @@ namespace Arkanoid {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"Game";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->components = (gcnew System::ComponentModel::Container());
+			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(Game::typeid));
+			this->ball = (gcnew System::Windows::Forms::PictureBox());
+			this->timer = (gcnew System::Windows::Forms::Timer(this->components));
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->ball))->BeginInit();
+			this->SuspendLayout();
+			// 
+			// ball
+			// 
+			this->ball->BackColor = System::Drawing::Color::Transparent;
+			this->ball->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"ball.Image")));
+			this->ball->Location = System::Drawing::Point(322, 393);
+			this->ball->Name = L"ball";
+			this->ball->Size = System::Drawing::Size(25, 25);
+			this->ball->SizeMode = System::Windows::Forms::PictureBoxSizeMode::AutoSize;
+			this->ball->TabIndex = 0;
+			this->ball->TabStop = false;
+			// 
+			// timer
+			// 
+			this->timer->Enabled = true;
+			this->timer->Interval = 20;
+			this->timer->Tick += gcnew System::EventHandler(this, &Game::timer1_Tick);
+			// 
+			// Game
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"$this.BackgroundImage")));
+			this->ClientSize = System::Drawing::Size(699, 501);
+			this->Controls->Add(this->ball);
+			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::FixedSingle;
+			this->MaximizeBox = false;
+			this->Name = L"Game";
+			this->Text = L"Game - Arkanoid";
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->ball))->EndInit();
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
 		}
+
+
+		// move ball
+		int moveX = 5;
+		int moveY = 5;
+
 #pragma endregion
+	private: System::Void timer1_Tick(System::Object^ sender, System::EventArgs^ e) {
+		// move ball
+		ball->Left += moveX;
+		ball->Top += moveY;
+		
+		//bouncing the ball
+		if (ball->Left >= Game::Width - ball->Width - 10) moveX = -moveX;
+
+		if (ball->Top >= Game::Height - ball->Height) moveY = -moveY;
+
+		if (ball->Top <= 75) moveY = -moveY;
+
+		if (ball->Left <= 5) moveX = -moveX;
+
+
+	}
 	};
 }
