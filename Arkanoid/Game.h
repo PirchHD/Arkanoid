f369653a7fa0932cf03210537d78f508cb839654
Prochal Szymon <34886043+PirchHD@users.noreply.github.com>
2020-08-02 17:35:32 +0200

    Restart game

--- Arkanoid/Game.h
+++ Arkanoid/Game.h
@@ -41,6 +41,9 @@ namespace Arkanoid {
 	private: System::Windows::Forms::PictureBox^ platform;
 	private: System::Windows::Forms::Label^ lblPoint;
 	private: System::Windows::Forms::Label^ lbllives;
+	private: System::Windows::Forms::PictureBox^ picend;
+	private: System::Windows::Forms::PictureBox^ picagain;
+	private: System::Windows::Forms::PictureBox^ piclose;
 
 	protected:
 
@@ -66,8 +69,14 @@ namespace Arkanoid {
 			this->platform = (gcnew System::Windows::Forms::PictureBox());
 			this->lblPoint = (gcnew System::Windows::Forms::Label());
 			this->lbllives = (gcnew System::Windows::Forms::Label());
+			this->picend = (gcnew System::Windows::Forms::PictureBox());
+			this->picagain = (gcnew System::Windows::Forms::PictureBox());
+			this->piclose = (gcnew System::Windows::Forms::PictureBox());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->ball))->BeginInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->platform))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->picend))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->picagain))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->piclose))->BeginInit();
 			this->SuspendLayout();
 			// 
 			// ball
@@ -125,12 +134,56 @@ namespace Arkanoid {
 			this->lbllives->TabIndex = 3;
 			this->lbllives->Text = L"3";
 			// 
+			// picend
+			// 
+			this->picend->BackColor = System::Drawing::Color::Transparent;
+			this->picend->Cursor = System::Windows::Forms::Cursors::Hand;
+			this->picend->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"picend.Image")));
+			this->picend->Location = System::Drawing::Point(91, 211);
+			this->picend->Name = L"picend";
+			this->picend->Size = System::Drawing::Size(199, 85);
+			this->picend->SizeMode = System::Windows::Forms::PictureBoxSizeMode::AutoSize;
+			this->picend->TabIndex = 4;
+			this->picend->TabStop = false;
+			this->picend->Visible = false;
+			this->picend->Click += gcnew System::EventHandler(this, &Game::picend_Click);
+			// 
+			// picagain
+			// 
+			this->picagain->BackColor = System::Drawing::Color::Transparent;
+			this->picagain->Cursor = System::Windows::Forms::Cursors::Hand;
+			this->picagain->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"picagain.Image")));
+			this->picagain->Location = System::Drawing::Point(400, 210);
+			this->picagain->Name = L"picagain";
+			this->picagain->Size = System::Drawing::Size(199, 86);
+			this->picagain->SizeMode = System::Windows::Forms::PictureBoxSizeMode::AutoSize;
+			this->picagain->TabIndex = 5;
+			this->picagain->TabStop = false;
+			this->picagain->Visible = false;
+			this->picagain->Click += gcnew System::EventHandler(this, &Game::picagain_Click);
+			// 
+			// piclose
+			// 
+			this->piclose->BackColor = System::Drawing::Color::Transparent;
+			this->piclose->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"piclose.Image")));
+			this->piclose->Location = System::Drawing::Point(451, 302);
+			this->piclose->Name = L"piclose";
+			this->piclose->Size = System::Drawing::Size(251, 200);
+			this->piclose->SizeMode = System::Windows::Forms::PictureBoxSizeMode::AutoSize;
+			this->piclose->TabIndex = 6;
+			this->piclose->TabStop = false;
+			this->piclose->Visible = false;
+			this->piclose->Click += gcnew System::EventHandler(this, &Game::piclose_Click);
+			// 
 			// Game
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"$this.BackgroundImage")));
 			this->ClientSize = System::Drawing::Size(699, 501);
+			this->Controls->Add(this->piclose);
+			this->Controls->Add(this->picagain);
+			this->Controls->Add(this->picend);
 			this->Controls->Add(this->lbllives);
 			this->Controls->Add(this->lblPoint);
 			this->Controls->Add(this->platform);
@@ -143,6 +196,9 @@ namespace Arkanoid {
 			this->KeyUp += gcnew System::Windows::Forms::KeyEventHandler(this, &Game::Game_KeyUp);
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->ball))->EndInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->platform))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->picend))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->picagain))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->piclose))->EndInit();
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
@@ -192,20 +248,31 @@ namespace Arkanoid {
 		}
 		// if u lose yr ball(s) ;)
 		else if (ball->Top >= platform -> Top + 5) {
+
 			timer->Enabled = false;
 			ball->Enabled = false;
-			MessageBox::Show("U lose your balls ;)) ? ","Try if u can", MessageBoxButtons::OK);
-			ball->Top = platform->Top - ball->Height - 2;
-			ball -> Left = platform->Left + platform->Width/2;
-			moveX = 0; moveY = 0;
-			timer->Enabled = true;
-			ball->Enabled = true;
+
 			points -= 100;
 			lives -= 1;
-			lblPoint->Text = "" + points;
-			lbllives->Text = "" + lives;
 
-			block = false;
+			lblPoint->Text = "" + points;
+
+			if (lives > 0) {
+				MessageBox::Show("U lose your balls ;)) ? ", "Try if u can", MessageBoxButtons::OK);
+				ball->Top = platform->Top - ball->Height - 2;
+				ball->Left = platform->Left + platform->Width / 2;
+				moveX = 0; moveY = 0;
+				timer->Enabled = true;
+				ball->Enabled = true;
+				lbllives->Text = "" + lives;
+				block = false;
+			}
+			else {
+				lbllives->Text = ";(";
+				piclose->Visible = true;
+				picagain->Visible = true;
+				picend->Visible = true;
+			}
 			
 		}
 	}
@@ -228,6 +295,33 @@ private: System::Void platform_Click(System::Object^ sender, System::EventArgs^ 
 }
 private: System::Void Game_KeyUp(System::Object^ sender, System::Windows::Forms::KeyEventArgs^ e) {
 	Direction = 'S';
+}
+private: System::Void picend_Click(System::Object^ sender, System::EventArgs^ e) {
+	this->Close();
+}
+private: System::Void piclose_Click(System::Object^ sender, System::EventArgs^ e) {
+}
+
+private: System::Void picagain_Click(System::Object^ sender, System::EventArgs^ e) {
+	lives = 3;
+	points = 0;
+
+	lbllives->Text = "" + lives;
+	lblPoint->Text = "" + points;
+
+	piclose->Visible = false;
+	picagain->Visible = false;
+	picend->Visible = false;
+
+	ball->Top = platform->Top - ball->Height - 2;
+	ball->Left = platform->Left + platform->Width / 2;
+
+	ball->Visible = true;
+	timer->Enabled = true;
+	
+	moveX = 0; moveY = 0;
+
+	block = false;
 }
 };
 }
